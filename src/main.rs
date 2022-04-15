ca20ad82788b2e77c903b87427119cb200bcb998
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-15 17:14:06 +0200

    Detect invalid chars in username

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,29 @@ struct Record {
     username: String
 }
 
+fn valid_username(username: &str) -> bool {
+    let invalid_chars = ["&", "[", "]", "(", ")", "%","^", "*", ";", ":", "\\", "/", "|", "%", "!"];
+
+    for invalid_char in invalid_chars {
+        if username.contains(invalid_char) {
+            //println!("Invalid char: {:?} for username: {:?}", invalid_char, username);
+            return false;
+        }
+    }
+
+
+    return true;
+}
+
 fn read_csv() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
+    for (idx, result) in rdr.records().enumerate() {
         let csv_record = result?;
         let record: Record = csv_record.deserialize(None)?;
-        println!("{:?}", record.email);
-        println!("{:?}", record.username);
+        //println!("{:?}", record.email);
+        if !valid_username(&record.username) {
+            println!("Line {:?} : Username {:?} is invalid", idx + 1, record.username)
+        }
     }
     Ok(())
 }
